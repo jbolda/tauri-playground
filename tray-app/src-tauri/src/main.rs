d4ad78e833dac2f7876e13be324cce26cb907118
Jacob Bolda <me@jacobbolda.com>
2023-10-03 22:43:52 -0500

    tauri v2 tray app
    https://www.youtube.com/live/MhHeJW7xBrY?si=da8Ms7cZv1o4yCRb

--- tray-app/src-tauri/src/main.rs
+++ tray-app/src-tauri/src/main.rs
@@ -3,83 +3,89 @@
   windows_subsystem = "windows"
 )]
 
+use std::path::PathBuf;
+use tauri::menu::{MenuBuilder, MenuItemBuilder};
+use tauri::tray::{ClickType, TrayIconBuilder};
+use tauri::Icon;
 use tauri::Manager;
-use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 
 use sysinfo::{System, SystemExt};
 
 fn main() {
-  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-  let show = CustomMenuItem::new("show".to_string(), "Show");
-  let tray_menu = SystemTrayMenu::new()
-    .add_item(show)
-    .add_item(hide)
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(quit);
+  // let quit = MenuItem::new("quit".to_string(), "Quit");
+  // let hide = MenuItem::new("hide".to_string(), "Hide");
+  // let show = MenuItem::new("show".to_string(), "Show");
+  // let tray_menu = SystemTrayMenu::new()
+  //   .add_item(show)
+  //   .add_item(hide)
+  //   .add_native_item(SystemTrayMenuItem::Separator)
+  //   .add_item(quit);
 
-  let system_tray = SystemTray::new().with_menu(tray_menu);
+  // let system_tray = SystemTray::new().with_menu(tray_menu);
 
   tauri::Builder::default()
     .setup(|app| {
-      let window = app.get_window("main").unwrap();
-      Ok(window.hide().unwrap())
+      let quit = MenuItemBuilder::new("Quit").id("quit").build(app);
+      let hide = MenuItemBuilder::new("Hide").id("hide").build(app);
+      let show = MenuItemBuilder::new("Show").id("show").build(app);
+      // we could opt handle an error case better than calling unwrap
+      let menu = MenuBuilder::new(app)
+        .items(&[&quit, &hide, &show])
+        .build()
+        .unwrap();
+
+      // best to use 32 pixel for tray icon
+      let tray_icon = Icon::File(PathBuf::from("./icons/32x32.png"));
+      let tray = TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&menu)
+        .on_menu_event(|app, event| match event.id().as_ref() {
+          "quit" => {
+            std::process::exit(0);
+          }
+          "hide" => {
+            let window = app.get_window("main").unwrap();
+            window.hide().unwrap();
+          }
+          "show" => {
+            let window = app.get_window("main").unwrap();
+            window.show().unwrap();
+          }
+          _ => {}
+        })
+        .on_tray_event(|tray_icon, event| match event.click_type {
+          ClickType::Left => {
+            dbg!("system tray received a left click");
+            let window = tray_icon.app_handle().get_window("main").unwrap();
+            window.show().unwrap();
+            let logical_size = tauri::LogicalSize::<f64> {
+              width: 300.00,
+              height: 400.00,
+            };
+            let logical_s = tauri::Size::Logical(logical_size);
+            window.set_size(logical_s);
+            let logical_position = tauri::LogicalPosition::<f64> {
+              x: event.x - logical_size.width,
+              y: event.y - logical_size.height - 70.,
+            };
+            let logical_pos: tauri::Position = tauri::Position::Logical(logical_position);
+            window.set_position(logical_pos);
+            window.set_focus();
+          }
+          ClickType::Right => {
+            dbg!("system tray received a right click");
+            let window = tray_icon.app_handle().get_window("main").unwrap();
+            window.hide().unwrap();
+          }
+          ClickType::Double => {
+            dbg!("system tray received a double click");
+          }
+        })
+        .build(app);
+
+      Ok(())
     })
     .invoke_handler(tauri::generate_handler![get_system_memory])
-    .system_tray(system_tray)
-    .on_system_tray_event(move |app, event| match event {
-      SystemTrayEvent::LeftClick {
-        position, size: _, ..
-      } => {
-        dbg!("system tray received a left click");
-        let window = app.get_window("main").unwrap();
-        window.show().unwrap();
-        let logical_size = tauri::LogicalSize::<f64> {
-          width: 300.00,
-          height: 400.00,
-        };
-        let logical_s = tauri::Size::Logical(logical_size);
-        window.set_size(logical_s);
-        let logical_position = tauri::LogicalPosition::<f64> {
-          x: position.x - logical_size.width,
-          y: position.y - logical_size.height - 70.,
-        };
-        let logical_pos: tauri::Position = tauri::Position::Logical(logical_position);
-        window.set_position(logical_pos);
-        window.set_focus();
-      }
-      SystemTrayEvent::RightClick {
-        position: _,
-        size: _,
-        ..
-      } => {
-        dbg!("system tray received a right click");
-        let window = app.get_window("main").unwrap();
-        window.hide().unwrap();
-      }
-      SystemTrayEvent::DoubleClick {
-        position: _,
-        size: _,
-        ..
-      } => {
-        dbg!("system tray received a double click");
-      }
-      SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-        "quit" => {
-          std::process::exit(0);
-        }
-        "hide" => {
-          let window = app.get_window("main").unwrap();
-          window.hide().unwrap();
-        }
-        "show" => {
-          let window = app.get_window("main").unwrap();
-          window.show().unwrap();
-        }
-        _ => {}
-      },
-      _ => {}
-    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- tray-app/src/main.jsx
+++ tray-app/src/main.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Menu, MenuItem } from "@tauri-apps/api/menu";
+
+// it is _very_ alpha and only worked on stream
+//  based on code in a PR
+async function createMenu() {
+  let i = 0;
+  let menu;
+  const boop = await MenuItem.new({
+    text: `Boop${i}`,
+    action: async () => {
+      console.log(`clicked boop ${i}`);
+      i++;
+      const newMenuItem = await MenuItem.new({
+        text: `Boop${i}`,
+        action: () => console.log(`clicked boop ${i}`),
+      });
+      menu.append(newMenuItem);
+    },
+  });
+  menu = await Menu.new({ items: [boop] });
+
+  await menu.setAsAppMenu();
+}
+createMenu();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
